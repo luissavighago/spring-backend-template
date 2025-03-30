9c08e26506e37b0a8650fd3db90e989fca3e2a3a
luissavighago <55593826+luissavighago@users.noreply.github.com>
2025-03-30 15:06:17 -0300

    feat: initial commit

---
+++ src/main/java/com/template/spring_backend_template/SpringBackendTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.template.spring_backend_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBackendTemplateApplication.class, args);
+	}
+
+}
